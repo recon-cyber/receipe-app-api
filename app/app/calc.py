85a3059dcdda9492a37eecf6788be188e9d5d213
Youssef Houmazen <chainzen@Youssefs-MacBook-Air.local>
2024-02-25 11:54:23 +0100

    fixing lint 3

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,6 @@
 """ this is simple calc test"""
 
 
-def add(x,y):
-  """ Add x and y return."""
-  return x + y
+def add(x, y):
+    """Add x and y return."""
+    return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,10 @@
-
 from django.test import SimpleTestCase
 from app import calc
 
 
 class CalcTests(SimpleTestCase):
-  
-  def test_add_numbers(self):
-    res = calc.add(6,5)
-    
-    self.assertEqual(res,11)
+
+    def test_add_numbers(self):
+        res = calc.add(6, 5)
+
+        self.assertEqual(res, 11)
